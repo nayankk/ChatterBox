767dc2197954211932b939f1c76578b7cafdb2bf
Nayan Kumar K <nayankk@motorola.com>
2016-02-17 19:22:31 -0800

    Initial

---
+++ app/src/main/java/xc0ffee/chatterbox/ChatApplication.java
@@ -0,0 +1,20 @@
+package xc0ffee.chatterbox;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by qtc746 on 2/17/16.
+ */
+public class ChatApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("93bd0b59a90d46b1999e484431b83f41")
+                .server("https://simplechatclient.herokuapp.com/parse/").build());
+
+    }
+}
---
+++ app/src/main/java/xc0ffee/chatterbox/ChatterBoxActivity.java
@@ -0,0 +1,89 @@
+package xc0ffee.chatterbox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class ChatterBoxActivity extends AppCompatActivity {
+
+    static final String TAG = ChatterBoxActivity.class.getSimpleName();
+
+    private TextView etMessage;
+    private Button btSend;
+
+    private static final String USER_ID_KEY = "user-id";
+    private static final String BODY_KEY = "body";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatter_box);
+
+        // User login
+        if (ParseUser.getCurrentUser() != null) { // start with existing user
+            startWithCurrentUser();
+        } else { // If not logged in, login as a new anonymous user
+            login();
+        }
+
+    }
+
+    // Get the userId from the cached currentUser object
+    void startWithCurrentUser() {
+        setupMessagePosting();
+    }
+
+    // Create an anonymous user using ParseAnonymousUtils and set sUserId
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else {
+                    startWithCurrentUser();
+                }
+            }
+        });
+    }
+
+
+    // Setup button event handler which posts the entered message to Parse
+    void setupMessagePosting() {
+        // Find the text field and button
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        btSend = (Button) findViewById(R.id.btSend);
+        // When send button is clicked, create message object on Parse
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+                message.put(BODY_KEY, data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(ChatterBoxActivity.this, "Successfully created message on Parse",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                etMessage.setText(null);
+            }
+        });
+    }
+
+}
